10bcbda2036d2842c70c76dc25c31833fa74109f
jeandrei <jeandreiwalter@gmail.com>
2022-03-17 18:51:43 +0000

    com validacao jquery validation

---
+++ bkp_dev/03_shareposts_jquery_validation/informativo.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * ARQUIVOS QUE DEVEM SER ALTERADOS PARA INICIAR UM NOVO PROJETO
+ * Alterar as configurações do banco de dados no arquivo 
+ * mvc/app/config.php
+ * Alterar a linha dentro do arquivo .htaccess para o diretório do site site/public
+ * public/.htaccess * 
+ * RewriteBase /site/public
+ * 
+ * 
+ * ENVIO DE EMAIL
+ * Arquivos relacionados com o envio do email de recuperação de senha
+ * config/config.php
+ * helpers/functions.php função validaemail
+ * controllers/Users.php método enviasenha
+ * models/User.php método sendemail
+ * views/users/enviasenha.php
+ * inc\PHPMailer-master
+ * 
+ * 
+ * JQUERY VALIDATOR
+ * Para funcionar o jquery validator o formulário tem que ter id e no script tem que
+ * ser o mesmo id exemplo veja \views\users\register
+ * exemplo formulário: <form id="register"
+ *  e no script: $('#register').validate({
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+    
+   
