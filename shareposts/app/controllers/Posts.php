03945990c3626129fd7293e8660ca541c88cfad2
jeandrei <jeandreiwalter@gmail.com>
2022-05-03 14:41:50 +0000

    post com imagens

--- shareposts/app/controllers/Posts.php
+++ shareposts/app/controllers/Posts.php
@@ -238,6 +238,7 @@
             
             $data = [
                 'post' => $post,
+                'files'=> $this->postModel->getFilePostById($id),
                 'user' => $user
             ];
 
--- shareposts/app/views/posts/index.php
+++ shareposts/app/views/posts/index.php
@@ -1,36 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php';?>
-<style>
-/* style do carrocel */
-@media (min-width: 768px) {
-  .carousel-inner {
-    display: flex;
-  }
-  .carousel-item {
-    margin-right: 0;
-    flex: 0 0 33.333333%;
-    display: block;
-  }
-}
 
-.carousel-inner{
-    padding: 1em;
-}
-.card{
-    margin: 0 .5em;
-    box-shadow: 2px 6px 8px 0 rgba(22, 22, 26, 0.18);
-    border: none;
-}
-.carousel-control-prev, .carousel-control-next{
-    background-color: #e1e1e1;
-    width: 6vh;
-    height: 6vh;
-    border-radius: 50%;
-    top: 50%;
-    transform: translateY(-50%);
-}
-
-
-</style>
 <!-- post_message é o nome da menságem está lá no controller -->
 <?php flash('post_message'); ?>
 <!-- mb-3 marging bottom -->
@@ -44,44 +13,47 @@
         </a>
     </div>
 </div>
-
 <?php foreach($data['posts'] as $post) : ?>
     <div class="card card-body mb-3">
         <h4 class="card-title"><?php echo $post['title']; ?></h4>
         <div class="bg-light p-2 mb-3">
           Escrito por <?php echo $post['name']; ?> em <?php echo date('d/m/Y h:i:s', strtotime($post['created_at'])); ?>
         </div>
-        <p class="card-text"><?php echo $post['body']; ?></p> 
-        <!-- se existir algum arquivo -->
-        <?php if($post['files']) : ?>   
-            <!-- Monta o carrocel -->
-            <div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
-                <div class="carousel-inner">             
-                <?php $i=0;?>
-                <?php foreach($post['files'] as $file) : ?>                 
-                    <div class="carousel-item <?php echo ($i==0)?"active":""?>">
-                    <div class="card">
-                    <img src="data:image/jpeg;base64,<?php echo base64_encode($file["file"]); ?>" class="d-block w-100" alt="..." >
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $file['title'];?></h5>
-                            <p class="card-text"><?php echo $file['body'];?></p>                           
-                        </div>
-                    </div>                  
+        <p class="card-text"><?php echo $post['body']; ?></p>
+
+        <!-- ******************************************************************* -->
+        
+        <?php if($post['files']) : ?>
+            <!-- Gallery -->
+            <div class="row">                              
+                <?php for($i=0;$i<=2;$i++) : ?> 
+                    <!-- col -->  
+                    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">                        
+                        <!-- card -->
+                        <?php echo ($post['files'][$i]['title']) ? "<div class='card mb-3'>":""?>                        
+                            <img
+                            src="data:image/jpeg;base64,<?php echo base64_encode($post['files'][$i]["file"]); ?>" width="375" height="250"
+                            class="w-100 shadow-1-strong rounded mb-4"
+                            alt="Boat on Calm Water"
+                            />
+                            <?php if($file['title']) : ?>
+                                <!-- card body -->
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $post['files'][$i]['title'];?></h5>
+                                    <p class="card-text"><?php echo $post['files'][$i]['body'];?></p>
+                                </div>
+                                <!-- card body -->
+                            <?php endif; ?>                          
+                        <?php echo ($post['files'][$i]['title']) ? "</div>":""?>
+                        <!-- card -->                        
                     </div>
-                <?php $i=1;?>
-                <?php endforeach; ?>                
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                    <!-- col -->  
+                <?php endfor; ?>  
             </div>
-            <!-- fim carrocel -->
-        <?php endif;?>
+            <!-- row -->
+        <?php endif; ?>    
+       
+
         <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post['id']; ?>" class="btn btn-dark">
         Mais</a>
 
@@ -90,42 +62,4 @@
 <?php endforeach; ?>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
-
-<script>
-/* jquery do carrocel 
-fonte: https://codingyaar.com/bootstrap-carousel-multiple-items-increment-by-1/*/
-var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-var cardWidth = $(".carousel-item").width();
-var scrollPosition = 0;
-
-$(".carousel-control-next").on("click", function () {
-  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-    scrollPosition += cardWidth;  //update scroll position
-    $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-  }
-});
-
-$(".carousel-control-prev").on("click", function () {
-  if (scrollPosition > 0) {
-    scrollPosition -= cardWidth;
-    $(".carousel-inner").animate(
-      { scrollLeft: scrollPosition },
-      600
-    );
-  }
-});
-
-var multipleCardCarousel = document.querySelector(
-  "#carouselExampleControls"
-);
-if (window.matchMedia("(min-width: 768px)").matches) {
-  //rest of the code
-  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-    interval: false
-  });
-} else {
-  $(multipleCardCarousel).addClass("slide");
-}
-
-</script>
 
